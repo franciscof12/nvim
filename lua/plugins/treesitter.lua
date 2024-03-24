91a293c0f77b9d06619d28ddf158d960f13abfc6
Francisco <franciscoferrefc@gmail.com>
2024-03-24 05:48:11 +0100

    [FEAT] featurings..

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require("nvim-treesitter.configs")
     configs.setup({
-      ensure_installed = { "lua", "javascript", "html", "go" },
+      auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
     })
