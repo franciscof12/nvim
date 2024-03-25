52b36d42de25eca604baf6486ba10b88a9745fd9
Francisco <franciscoferrefc@gmail.com>
2024-03-26 00:54:07 +0100

    [FEAT] tmux navigation

--- lua/nv-configuration.lua
+++ lua/nv-configuration.lua
@@ -2,6 +2,15 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+local keymap = vim.keymap -- alias for vim.keymap.set
+
+-- Keymaps for nvim
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>sa", ":wa<CR>", {})
-vim.keymap.set("n", "<C-n>", ":set relativenumber<CR>", {})
+keymap.set("n", "<leader>sa", ":wa<CR>", {})
+keymap.set("n", "<C-n>", ":set relativenumber<CR>", {})
+
+-- split windows
+keymap.set("n", "<leader>sh", ":split<CR>", {})
+keymap.set("n", "<leader>sv", ":vsplit<CR>", {})
+-- close current split windows
+keymap.set("n", "<leader>q", ":close<CR>", {})
