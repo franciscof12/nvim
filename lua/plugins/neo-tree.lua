9d060e71c49627262beec3a74bebbfafd2efece6
Francisco <franciscoferrefc@gmail.com>
2024-03-25 14:39:00 +0100

    [FEAT] unhidde dotfiles

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,23 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
   config = function()
+    require("neo-tree").setup({
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    })
     vim.keymap.set("n", "<C-s>", ":Neotree filesystem reveal right<CR>", {})
   end,
 }
