a35eca656d925cc927678a78634b16fe00f3de82
Francisco <franciscoferrefc@gmail.com>
2024-03-26 13:20:37 +0100

    [FEAT] new color scheme

--- lua/nv-configuration.lua
+++ lua/nv-configuration.lua
@@ -7,10 +7,17 @@ local keymap = vim.keymap -- alias for vim.keymap.set
 -- Keymaps for nvim
 vim.g.mapleader = " "
 keymap.set("n", "<leader>sa", ":wa<CR>", {})
-keymap.set("n", "<C-n>", ":set relativenumber<CR>", {})
+-- set relative and absolute line numbers
+keymap.set("n", "<C-n>", ":set relativenumber!<CR>", {})
 
 -- split windows
 keymap.set("n", "<leader>sh", ":split<CR>", {})
 keymap.set("n", "<leader>sv", ":vsplit<CR>", {})
 -- close current split windows
 keymap.set("n", "<leader>q", ":close<CR>", {})
+
+-- Gvdiffsplit!
+keymap.set("n", "<leader>gv", ":Gvdiffsplit!<CR>", {})
+
+-- wqa 
+keymap.set("n", "<leader>qe", ":wqa<CR>", {})
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,23 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    -- config = function()
+    -- vim.cmd.colorscheme("catppuccin-frappe")
+    --end,
+  },
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+      })
+    end,
+
+    init = function()
+      vim.cmd("colorscheme poimandres")
+    end,
+  },
 }
